0b97ca1adab72f3ddf78aa488073ac78fc4cc438
jorge <jorge@10.0.2.15>
2021-04-09 17:55:50 +0200

    Esta es la prueba 2

---
+++ src/Pruebagit/Prueba2.java
@@ -0,0 +1,11 @@
+package Pruebagit;
+
+public class Prueba2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esta es la prueba2");
+
+	}
+
+}
