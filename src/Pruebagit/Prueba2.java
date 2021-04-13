1cd3216c574e2a85f7a0f220a1511f9a93949633
AngelGarciaMarco <82109986+AngelGarciaMarco@users.noreply.github.com>
2021-04-13 19:03:30 +0200

    Update Prueba2.java

--- src/Pruebagit/Prueba2.java
+++ src/Pruebagit/Prueba2.java
@@ -5,6 +5,7 @@ public class Prueba2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esta es la prueba");
+		System.out.println("Estoy desde eclipse");
 	}
 
 }
