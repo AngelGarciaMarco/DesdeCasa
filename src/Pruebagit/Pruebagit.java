fcad4c0e220ba29c5ccb1589bc42e184a73c7550
jorge <jorge@10.0.2.15>
2021-04-13 19:19:52 +0200

    actu

--- src/Pruebagit/Pruebagit.java
+++ src/Pruebagit/Pruebagit.java
@@ -5,6 +5,7 @@ public class Pruebagit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Este es un nuevo .java");
+		System.out.println("Esto es una modificacion desde eclipse");
 	}
 
 }
